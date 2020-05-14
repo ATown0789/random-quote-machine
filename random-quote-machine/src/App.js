db149e9814f650680062e7960861de6c5aca9a3e
Anthony Collini <collini.anthony@gmail.com>
2020-05-14 12:18:26 -0500

    Added FCC test and attempt to add background styling

--- random-quote-machine/src/App.js
+++ random-quote-machine/src/App.js
@@ -13,31 +13,29 @@ class App extends React.Component {
 			author: 'Will Durant', 
 			data: null,
 			colors: [
-				"#16a085",
-				"#27ae60",
-				"#2c3e50",
-				"#f39c12",
-				"#e74c3c",
-				"#9b59b6",
-				"#FB6964",
-				"#342224",
-				"#472E32",
-				"#BDBB99",
-				"#77B1A9",
-				"#73A857"
+				"#16a085 !important",
+				"#27ae60 !important",
+				"#2c3e50 !important",
+				"#f39c12 !important",
+				"#e74c3c !important",
+				"#9b59b6 !important",
+				"#FB6964 !important",
+				"#342224 !important",
+				"#472E32 !important",
+				"#BDBB99 !important",
+				"#77B1A9 !important",
+				"#73A857 !important"
 			]
 		}
 		
 		this.addNewQuote = this.addNewQuote.bind(this)
 	}
 	
-	addNewQuote(newQuote){
-		
+	addNewQuote(newQuote){	
 		this.setState({
 			quoteText: newQuote.text,
 			author: newQuote.author
-		})
-		
+		})	
 	}
 	
 	componentDidMount(){
@@ -46,18 +44,24 @@ class App extends React.Component {
 			.then(data => this.setState({data}));
 	}
 	
+	
+	
 	render(){
-		console.log(this.state.data)
+		let backgroundColor = this.state.colors[Math.floor(Math.random()*12)];
+		
 	return (
-		<div id = "quote-box">
-			<div className = 'textCont'>
-				<h1>Random Quote Machine</h1>
-				<QuoteText state = {this.state}/>
-			</div>
-			<div className = 'buttonTweetCont'>
-				<a id = "tweet-quote" href='https://twitter.com/intent/tweet'>Tweet Quote
-				</a>
-				<NewQuoteButton state = {this.state} addNewQuote = {this.addNewQuote}/>
+		<div className = "wrapper">
+			<div id = "quote-box">
+				<div className = 'textCont'>
+					<h1>Random Quote Machine</h1>
+					<QuoteText state = {this.state}/>
+				</div>
+				<div className = 'buttonTweetCont'>
+					<a id = "tweet-quote" href={'https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text='+ encodeURIComponent('"' + this.state.quoteText + '"\r\n- ' + this.state.author)}>
+					<i className="fab fa-twitter"></i>
+					</a>
+					<NewQuoteButton state = {this.state} addNewQuote = {this.addNewQuote}/>
+				</div>
 			</div>
 		</div>
 			
