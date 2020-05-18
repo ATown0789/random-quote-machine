9bcaba34ddfe5a51b262bb048a3a18afee80fa18
Anthony Collini <collini.anthony@gmail.com>
2020-05-18 18:38:50 -0500

    Trying to add transitions to #quote-box

--- src/App.js
+++ src/App.js
@@ -2,7 +2,9 @@ import React from 'react';
 import './App.css';
 import QuoteText from './components/QuoteText';
 import NewQuoteButton from './components/NewQuoteButton';
+import {TransitionGroup} from 'react-transition-group';
 
+let indexCount = 0;
 
 class App extends React.Component {
 	constructor(props){
@@ -12,29 +14,40 @@ class App extends React.Component {
 			quoteText: 'We are what we repeatedly do.\n Excellence, then,\n is not an act, but a habit.',
 			author: 'Will Durant', 
 			data: null,
+			style:{},
 			colors: [
-				"#16a085 !important",
-				"#27ae60 !important",
-				"#2c3e50 !important",
-				"#f39c12 !important",
-				"#e74c3c !important",
-				"#9b59b6 !important",
-				"#FB6964 !important",
-				"#342224 !important",
-				"#472E32 !important",
-				"#BDBB99 !important",
-				"#77B1A9 !important",
-				"#73A857 !important"
+				"#16a085",
+				"#27ae60",
+				"#2c3e50",
+				"#f39c12",
+				"#e74c3c",
+				"#9b59b6",
+				"#FB6964",
+				"#342224",
+				"#472E32",
+				"#BDBB99",
+				"#77B1A9",
+				"#73A857"
 			]
 		}
 		
 		this.addNewQuote = this.addNewQuote.bind(this)
 	}
 	
-	addNewQuote(newQuote){	
+	
+	addNewQuote(newQuote){
+		if(indexCount < this.state.colors.length)
+		indexCount++;
+		else
+		indexCount = 0;
+		
 		this.setState({
 			quoteText: newQuote.text,
-			author: newQuote.author
+			author: newQuote.author,
+			style: {backgroundColor: this.state.colors[indexCount],
+					transition: "all 1s ease",
+					WebkitTransition: "all 1s ease",
+					MozTransition: "all 1s ease"}
 		})	
 	}
 	
@@ -46,12 +59,12 @@ class App extends React.Component {
 	
 	
 	
-	render(){
-		let backgroundColor = this.state.colors[Math.floor(Math.random()*12)];
-		
+	
+	
+	render(){		
 	return (
-		<div className = "wrapper">
-			<div id = "quote-box">
+		<div className = "wrapper" style = {this.state.style}>
+			<div className="transition" id = "quote-box">
 				<div className = 'textCont'>
 					<h1>Random Quote Machine</h1>
 					<QuoteText state = {this.state}/>
